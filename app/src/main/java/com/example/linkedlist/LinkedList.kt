64982b3d5e5fd8311c869324728578289a648a6f
Han-JeongHyeon <fldkej131@gmail.com>
2022-10-20 17:39:56 +0900

    addAt

--- app/src/main/java/com/example/linkedlist/LinkedList.kt
+++ app/src/main/java/com/example/linkedlist/LinkedList.kt
@@ -4,60 +4,119 @@ import org.w3c.dom.Node
 import java.util.*
 
 fun main() {
-    val linkedList = LinkedList<Int>()
+    val linkedList = LinkedList()
 
-    linkedList.add(0)
-
-    for (i in 1..10) {
-        linkedList.add(i)
+    for (i in 1..5) {
+        linkedList.addAll(i)
     }
-    linkedList.desc()
 
-    linkedList.delete(0)
-    linkedList.delete(10)
-    linkedList.desc()
+    linkedList.addAt(2,10)
+    linkedList.delete(1)
+    linkedList.deleteAt(0)
+
+    linkedList.printAll()
 }
 
-class LinkedList<E> {
-    private var head: Node<E>? = null
+class LinkedList {
+    private var head: Node? = null
+    var size: Int = 0
+    private var tail: Node? = null
 
-    private inner class Node<E>(
-        var data: E,
-        var next: Node<E>? = null
-    )
-
-    private fun addFirst(item: E) {
-        head = Node(item)
+    inner class Node {
+        var next: Node? = null
+        var item: Int? = null
     }
 
-    fun add(item: E) {
-        if (head == null) addFirst(item)
-        else {
-            var node = head
-            while (node?.next != null) node = node.next
-            node?.next = Node(item)
+    private fun addFirst(item: Int) {
+        val newNode = Node().apply {
+            this.item = item
+            this.next = head
+        }
+        head = newNode
+        if (head?.next == null) {
+            tail = head
+        }
+        size++
+    }
+
+    private fun find(index: Int): Node? {
+        var node = head
+        for (i in 0 until index) {
+            node = node?.next ?: error("invalid node")
+        }
+        return node
+    }
+
+    fun addAt(index: Int, item: Int) {
+        val newNode = Node().apply {
+            this.item = item
+        }
+        val node = find(index)!!
+        val temp = node.next
+        newNode.next = temp
+        node.next = newNode
+
+        if (newNode.next == null){
+            tail = newNode
+        }
+        size++
+    }
+
+    fun addAll(item: Int) {
+        if (size == 0) {
+            addFirst(item)
+        } else {
+            val newNode = Node().apply {
+                this.item = item
+            }
+            tail?.next = newNode
+            tail = newNode
+            size++
         }
     }
 
-    fun delete(item: E) {
+    fun delete(item: Int) {
         if (head == null) return println("노드가 존재하지 않습니다.")
         else {
-            if (head?.data == item) {
+            if (head?.item == item) {
                 head = head?.next
             } else {
                 var node = head
-                while (node?.next?.data != item) node = node?.next
-                node?.next = node?.next?.next
+                while (node?.next?.item != item) node = node?.next
+                node.next = node.next?.next
             }
         }
     }
 
-    fun desc() {
+    fun removeFirst(): Int {
+        val node = head
+        head = node?.next
+        size--
+        return node?.item ?: error("invalid item")
+    }
+
+    fun deleteAt(index : Int): Int{
+        if (index > size - 1) {
+            error("invalid position")
+        }
+
+        return if (index == 0) {
+            removeFirst()
+        } else {
+            val node = find(index - 1)
+            val deletedNode = node?.next
+            node?.next = node?.next?.next
+            size--
+            deletedNode?.item ?: error("invalid item")
+        }
+    }
+
+    fun printAll() {
         var node = head
         while (node != null) {
-            print("${node.data} ")
+            print("${node.item} ")
             node = node.next
         }
-        println()
     }
+
 }
